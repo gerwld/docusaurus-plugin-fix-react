b3945c816028e12df665f24af3d3e375799e7859
P Jaworski <pjaworski.dev@gmail.com>
2024-02-27 20:44:16 +0100

    init: commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import path from "path"
+
+module.exports = () => ({
+  name: 'docusaurus-plugin-fix-react',
+  configureWebpack() {
+    return {
+      resolve: {
+        alias: {
+          // assuming root node_modules is in same directory as plugin (node_modules)
+          react: path.resolve('./react'),
+        },
+      },
+    };
+  },
+})
