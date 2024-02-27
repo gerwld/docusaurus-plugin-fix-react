ae42638bb260cb247b4130b7f81505dee3332616
P Jaworski <pjaworski.dev@gmail.com>
2024-02-27 21:40:02 +0100

    build: changed rel. path to absolute

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = () => ({
       resolve: {
         alias: {
           // assuming root node_modules is in same directory as plugin (node_modules)
-          react: path.resolve('./react'),
+          react: path.resolve('/node_modules/react'),
         },
       },
     };
